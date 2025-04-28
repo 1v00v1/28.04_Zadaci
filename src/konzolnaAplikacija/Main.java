dbe6c186f0fa5a5648eefc96dc5608c17e3838aa
Iv00vI <goran.ivo11@gmail.com>
2025-04-28 20:58:20 +0200

    a

--- src/konzolnaAplikacija/Main.java
+++ src/konzolnaAplikacija/Main.java
@@ -20,6 +20,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static konzolnaAplikacija.DatabaseConnection.createDataSource;
@@ -32,8 +34,7 @@ public class Main {
         do {
             printOptions();
             System.out.print("Vaš izbor: ");
-            int izbor = Integer.parseInt(s.nextLine().trim());
-            switch (izbor) {
+            switch (Integer.parseInt(s.nextLine().trim())) {
                 case 1 -> unosDržave(s, ds);
                 case 2 -> izmjenaDržave(s,ds);
                 case 3 -> brisanjeDržave(s,ds);
@@ -86,15 +87,19 @@ public class Main {
 
     }
     private static  void izmjenaDržave(Scanner s, DataSource ds){
+        Boolean ex = false;
+        String drzava;
         try (Connection connection = ds.getConnection()){
+
+
             Statement st = connection.createStatement();
-            System.out.print("Unesite ime države koju želite izmjeniti: ");
-            String drzava = s.nextLine().trim();
-            System.out.print("Unesite novu vrijednost: ");
-            String novaVrijednost = s.nextLine().trim();
-            String query = String.format("UPDATE Drzava SET Naziv ='%s' WHERE Naziv = '%s'",novaVrijednost,drzava);
-            st.executeUpdate(query);
-            st.close();
+
+                System.out.print("Unesite ime države koju želite izmjeniti: ");
+                 drzava = s.nextLine().trim();
+                ResultSet rs = st.executeQuery("SELECT Naziv FROM Drzava");
+                while (rs.next()) {
+                    System.out.println(rs.getString("Naziv"));
+                }
 
         } catch (SQLException e) {
             e.printStackTrace();
